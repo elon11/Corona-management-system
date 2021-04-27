5261d6d0d6c3b111a152fe356564177249e555bc
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:15:39 +0300

    Update SouthAfricanVariant.java

--- HWֹ1/src/Virus/SouthAfricanVariant.java
+++ HWֹ1/src/Virus/SouthAfricanVariant.java
@@ -1,10 +1,20 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * A class describing one of the types of virus originating in South Africa
+ */
 package Virus;
 import java.lang.Math;
 import Population.Person;
 import Population.*;
 import Simulation.Clock;
 
+
 public class SouthAfricanVariant implements IVirus {
+	
+	/*
+	 * @param p - A method that calculates the probability that the transferred person will be infected.
+	 */
 	public double ContagionProbability(Person p) {
 		double percent;
 		if(p.getAge()<=18)
@@ -14,6 +24,9 @@ public class SouthAfricanVariant implements IVirus {
 		return percent*p.ContagionProbability();
 	}
 	
+	/*
+	 * @param p - An help method that accepts a patient and returns the probability of death from his virus
+	 */
 	public double p_to_death(Sick p) {
 		if(p.getAge()<=18)
 			return 0.05;
@@ -21,7 +34,10 @@ public class SouthAfricanVariant implements IVirus {
 		
 	}
 	
-	//לעשות אינסטנס אוף בכל הוירוסים
+	/*
+	 * @param p1 and p2 - A method that accepts 2 people and results in pasting the other with the help of a formula and distance from the first person
+	 */
+	@Override
 	public boolean tryToContagion(Person p1, Person p2) {
 		double distance, min1;
 		if(p2 instanceof Healthy || p2 instanceof Vaccinated || p2 instanceof Convalescent)
@@ -41,6 +57,9 @@ public class SouthAfricanVariant implements IVirus {
 		return false;
 	}
 	
+	/*
+	 * @param p - A method that calculates the probability that the person being transferred will die from the disease
+	 */
 	public boolean tryToKill(Sick p) {
 		double x = p.getVirus().p_to_death(p);
 		double max1 = Math.max(0, x - 0.01*x* Math.pow(Clock.now() - p.getContagiousTime()-15, 2));
@@ -49,7 +68,14 @@ public class SouthAfricanVariant implements IVirus {
 	    	return true;
 	    return false;
 	}
+	
 	public String toString() {
 		return "SouthAfricanVariant";
+	}
+	
+	public boolean equals(Object o) {
+		if (!(o instanceof SouthAfricanVariant))
+		   return false;
+		return true;
 	}
 }
