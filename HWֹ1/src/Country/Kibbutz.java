bbf40840ee12492c86af8b1ed8d799f4a87a252d
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:05:15 +0300

    Update Kibbutz.java

--- HWֹ1/src/Country/Kibbutz.java
+++ HWֹ1/src/Country/Kibbutz.java
@@ -1,3 +1,8 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * The department describes a kibbutz on a map
+ */
 package Country;
 
 import java.util.List;
@@ -7,14 +12,19 @@ import Population.Person;
 
 public class Kibbutz extends Settlement {
 	
-	public Kibbutz(String name,Location location,RamzorColor color, List<Person> listp) {
-		super( name, location, color,  listp);
+	public Kibbutz(String name,Location location,RamzorColor color, List<Person> healthy,List<Person> sick,int max_people,int vaccine_doses,Settlement [] neighbors) {
+		super( name, location, color,  healthy, sick, max_people, vaccine_doses, neighbors);
 	}
+	
+	
+	/*
+	 * { @inheritDoc }
+	 */
 	@Override
 	public  RamzorColor CalculateRamzorGrade() {
 		double num;
 		
-		num = 0.45 + Math.pow(Math.pow(1.5, this.color.getValue(color))*(ContagiousPercent()-0.4),3);
+		num = 0.45 + Math.pow(Math.pow(1.5, this.color.getValue())*(ContagiousPercent()-0.4),3);
 		if(num <=0.4)
 			this.color = RamzorColor.Green;
 		if(num >0.4 && num<=0.6)
@@ -24,19 +34,16 @@ public class Kibbutz extends Settlement {
 		if(num >0.8)
 			this.color = RamzorColor.Red;
 		
-		this.setColor(color);
+		
 		return this.color;
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	public boolean equals(Object o) {
+		if (!(o instanceof Kibbutz))
+		   return false;
+		return true;
+	}
 	
 	
 	public String toString(){
