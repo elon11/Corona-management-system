ec9fa4c9ecc544d75f1e0dd3e41903c3ac2ab524
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:12:00 +0300

    Update Person.java

--- HWֹ1/src/Population/Person.java
+++ HWֹ1/src/Population/Person.java
@@ -1,4 +1,8 @@
-
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * A class that describes a person
+ */
 package Population;
 
 import Country.Settlement;
@@ -10,8 +14,14 @@ public abstract class Person {
 	protected Location location;
 	protected Settlement settlement;
 	
+	/*
+	 * An abstract method that returns a coefficient of probability for virus-independent infection
+	 */
 	public abstract double ContagionProbability(); 
 
+	/*
+	 * A method that returns a copy of data of the current person while turning it into a person with the transmitted virus.
+	 */
 	public  abstract Person Contagion(IVirus ivirus) ;
 
 	
@@ -23,7 +33,7 @@ public abstract class Person {
 	public void Setage(int age) {
 	this.age = age;
 	}
-	public int getAge() {
+	public int getAge() { 
 	return age;
 	}
 	public void Setlocation(Location location) {
@@ -42,6 +52,16 @@ public abstract class Person {
 	public String toString() {
 		
 	    return "age:"+this.age +", Location"+ this.location.toString() +", Settlement:"+ this.settlement.getName();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Person))
+		   return false;
+		Person n = (Person)o;
+		if(this.age==n.age && this.location.equals(n.location)&& this.settlement.equals(n.settlement))
+			return true;
+		return false; 
 	}
 
 }
