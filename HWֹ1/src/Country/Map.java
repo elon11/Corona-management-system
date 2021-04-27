655322903c135584b6e784f037da2fb602983cdc
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:05:56 +0300

    Update Map.java

--- HWֹ1/src/Country/Map.java
+++ HWֹ1/src/Country/Map.java
@@ -1,5 +1,10 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * The department describes the map of the area
+ */
 package Country;
-import Country.Settlement;
+
 
 public class Map {
 	private Settlement [] settlement;
@@ -9,7 +14,7 @@ public class Map {
 	public  Map()
 	{
 		settlement = new Settlement[size];
-		System.out.println("end constructor of map with this.size: "+ this.size); 
+		System.out.println("end constructor of map with this.size: "+ Map.size); 
 	}
 	
 	public Settlement[] getSettlement()
@@ -17,13 +22,34 @@ public class Map {
 		return settlement;
 	}
 	
+	public Settlement getSettlement_by_insex(int x)
+	{
+		return settlement[x];
+	}
+	
 	public void setSettlement(Settlement [] settlement)
 	{
 		this.settlement =  settlement;
 	}
 	
+	public static int getsize()
+	{
+		return size;
+	}
+	public static int getCurrentSize()
+	{
+		return index;
+	}	
+	
+	public void setsize(int size)
+	{
+		Map.size =  size;
+	}
 	
 	
+	/*
+	 * The method gets a place and adds it to the list of places on the map
+	 */
 	
 	public void AddSettlements(Settlement st)
 	{
@@ -33,7 +59,7 @@ public class Map {
 		//checking if the map is full mult the size of it and copy elements
 		if(index == size) 
 		{
-			size = size *2;
+			size = size + 1;
 			Settlement [] settlementTemp = new Settlement[size];
 			for(int i=0;i<settlement.length;i++)
 			{
