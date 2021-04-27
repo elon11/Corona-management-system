d7f7b35972bfcaacd68ff688ff81d365583ebe72
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:07:10 +0300

    Update RamzorColor.java

--- HWֹ1/src/Country/RamzorColor.java
+++ HWֹ1/src/Country/RamzorColor.java
@@ -1,17 +1,26 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ */
 package Country;
 
 public enum RamzorColor {
 
 	Green (0.4), Yellow (0.6), Orange (0.8), Red (1) ;
+	
 
 	RamzorColor(double d) {
-		// TODO Auto-generated constructor stub
-		//מה צריך לעשות פה?
+		this.d = d;
 	}
 	
-	public double getValue(RamzorColor color) {
-		return color.getValue(color);
-			
+    
+	
+	
+	
+	private final double d;
+
+	public double getValue() {
+		return d;
 	}
 
 	
