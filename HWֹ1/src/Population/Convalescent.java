060175b25a65264f91bb694fb77b150acd2e3e35
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:11:17 +0300

    Update Convalescent.java

--- HWֹ1/src/Population/Convalescent.java
+++ HWֹ1/src/Population/Convalescent.java
@@ -1,3 +1,8 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * The department came to describe a recovering person.
+ */
 package Population;
 import Location.Location;
 import Simulation.Clock;
@@ -22,15 +27,31 @@ private IVirus  virus;
 		this.virus = virus;
 	}
 	
+	/*
+	 * @inheritDoc
+	 */
+	@Override 
 	public  double ContagionProbability(){
 		return 0.2;
 	}
+	/*
+	 * @inheritDoc
+	 */
+	@Override
 	public  Person Contagion(IVirus ivirus) {
 		Sick p = new Sick( this.getAge(),this.Getlocation(),this.Getsettlement(), Clock.now(), ivirus);
 		return p;
+		
 	}
 	
     public String toString(){
     	return "virus:"+this.virus+ super.toString();
+	}
+    
+    public boolean equals(Object o) {
+		if (!(o instanceof Convalescent))
+		   return false;
+		Convalescent n = (Convalescent)o;
+		return this.virus.equals(n.virus)&& super.equals(o);
 	}
 }
