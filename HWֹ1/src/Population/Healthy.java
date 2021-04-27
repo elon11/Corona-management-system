383ebbadac99fed2288a4e484f9d3e99cb69db98
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:11:37 +0300

    Update Healthy.java

--- HWֹ1/src/Population/Healthy.java
+++ HWֹ1/src/Population/Healthy.java
@@ -1,3 +1,8 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * The department came to describe a Healthy person.
+ */
 package Population;
 import Simulation.Clock;
 import Location.Location;
@@ -13,23 +18,42 @@ public class Healthy extends Person {
 		
 	}
 	
-	
+	/*
+	 * A method that returns a copy of data of the current person while making him a vaccinated person.
+	 */
     public Person vaccinate() {
     	Vaccinated new_person = new Vaccinated(this.getAge(),this.Getlocation(),this.Getsettlement(),Clock.now());
     	return new_person;
+    	
 		
 	}
+    
+    /*
+	 * @inheritDoc
+	 */
+    @Override
     public  Person Contagion(IVirus ivirus) {
     	
     	Sick p = new Sick( this.getAge(),this.Getlocation(),this.Getsettlement(), Clock.now(), ivirus);
 		return p;
+		
     }
+    /*
+	 * @inheritDoc
+	 */
+    @Override
     public double ContagionProbability(){
 		return 1.0;
 	}
     @Override
     public String toString(){
     	return super.toString();
+	}
+    
+    public boolean equals(Object o) {
+		if (!(o instanceof Healthy))
+		   return false;
+		return  super.equals(o);
 	}
 
 }
