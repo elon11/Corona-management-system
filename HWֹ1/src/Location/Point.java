949e07a0e4f901d4d77c0e9a59267ce805802d22
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:10:20 +0300

    Update Point.java

--- HWֹ1/src/Location/Point.java
+++ HWֹ1/src/Location/Point.java
@@ -1,3 +1,8 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * The department describes a specific point in the field
+ */
 package Location;
 import java.lang.Math;
 
@@ -31,6 +36,15 @@ public class Point {
 		double distance;
 		distance = Math.sqrt(Math.pow((this.GetX()- point.GetX()), 2)+Math.pow((this.GetY()- point.GetY()), 2));
 		return distance;
+	}
+	
+	public boolean equals(Object o) {
+		if (!(o instanceof Point))
+		   return false;
+		Point n = (Point)o;
+		if(this.x == n.x && this.y == n.y )
+			return true;
+		return false;
 	}
 
 }
