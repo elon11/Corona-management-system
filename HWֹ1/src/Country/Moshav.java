42a133fad50583bf558e3e78828cce0d1ac17103
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-27 12:06:36 +0300

    Update Moshav.java

--- HWֹ1/src/Country/Moshav.java
+++ HWֹ1/src/Country/Moshav.java
@@ -1,4 +1,8 @@
-
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * The department describes a locality on a map
+ */
 package Country;
 
 import java.util.List;
@@ -7,15 +11,20 @@ import Location.Location;
 import Population.Person;
 
 public class Moshav extends Settlement {
-	public Moshav(String name,Location location,RamzorColor color, List<Person> listp) {
-		super( name, location, color,  listp);
+	public Moshav(String name,Location location,RamzorColor color, List<Person> healthy,List<Person> sick,int max_people,int vaccine_doses,Settlement [] neighbors) {
+		super( name, location, color,  healthy, sick, max_people, vaccine_doses, neighbors);
 	}
 	
+	
+
+	/*
+	 * { @inheritDoc } 
+	 */
 	@Override
 	public  RamzorColor CalculateRamzorGrade() {
 		double num;
 		
-		num = 0.3 + 3*Math.pow(Math.pow(1.2, this.color.getValue(color))*(ContagiousPercent()-0.35),5);
+		num = 0.3 + 3*Math.pow(Math.pow(1.2, this.color.getValue())*(ContagiousPercent()-0.35),5);
 		if(num <=0.4)
 			this.color = RamzorColor.Green;
 		if(num >0.4 && num<=0.6)
@@ -25,7 +34,7 @@ public class Moshav extends Settlement {
 		if(num >0.8)
 			this.color = RamzorColor.Red;
 		
-		this.setColor(color);
+		
 		return this.color;
 	}
 	
@@ -34,7 +43,11 @@ public class Moshav extends Settlement {
     	return  super.toString();
 	}
 	
-	
+	public boolean equals(Object o) {
+		if (!(o instanceof Moshav))
+		   return false;
+		return true;
+	}
 	
 	
 
