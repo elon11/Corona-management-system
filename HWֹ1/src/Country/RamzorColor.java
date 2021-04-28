0f5449b1d9299929eb04ebbc80cf8f5a4fb0e416
elon11 <73604571+elon11@users.noreply.github.com>
2021-04-28 11:04:30 +0300

    Update RamzorColor.java

--- HWֹ1/src/Country/RamzorColor.java
+++ HWֹ1/src/Country/RamzorColor.java
@@ -2,26 +2,45 @@
  * elon ifrah 207229931
  * yosi iluz 208510248
  */
+
 package Country;
+
+import java.awt.Color;
 
 public enum RamzorColor {
 
-	Green (0.4), Yellow (0.6), Orange (0.8), Red (1) ;
+	Green (0.4,Color.green,1.0), 
+	Yellow (0.6, Color.yellow,0.8), 
+	Orange (0.8, Color.orange,0.6),
+	Red (1.0,Color.red,0.4) ;
+	
 	
 
-	RamzorColor(double d) {
+	RamzorColor(double d,Color color,double number) {
 		this.d = d;
+		this.color= color;
+		this.number=number;
 	}
 	
-    
 	
-	
-	
-	private final double d;
+	RamzorColor(double d) {
+		this.d = d;
+		
+	}
+
+
+	private double d;
+	private Color color;
+	private double number;
 
 	public double getValue() {
 		return d;
 	}
 
-	
+	public Color getcolor() {
+		return color;
+	}
+	public double getnumber() {
+		return number;
+	}
 }
