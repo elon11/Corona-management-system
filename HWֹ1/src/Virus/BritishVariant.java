844be57306fc4914456cb8e45444bda51ba2a830
yossiluz <74909707+yossiluz@users.noreply.github.com>
2021-04-29 13:49:53 +0300

    Update BritishVariant.java

--- HWֹ1/src/Virus/BritishVariant.java
+++ HWֹ1/src/Virus/BritishVariant.java
@@ -1,24 +1,33 @@
+/*
+ * elon ifrah 207229931
+ * yosi iluz 208510248
+ * A class describing one of the types of virus originating in China
+ */
 package Virus;
 import Population.*;
 import Simulation.Clock;
 import java.lang.Math;
 import java.util.ArrayList;
-
 import java.util.List;
 
 
-public class ChineseVariant implements IVirus {
+public class BritishVariant implements IVirus {
+	
+	/*
+	 * @param p - A method that calculates the probability that the transferred person will be infected.
+	 */
 	private final static int min_coutage_time=5;
 	private static List<IVirus> variant = new ArrayList<IVirus>();
 	static {
-		variant.add(new ChineseVariant());
+		variant.add(new BritishVariant());
 	}
-	public ChineseVariant()
+	public BritishVariant()
 	{
 		/**
 		 * variants list contain the variant type that this variant can contaige
 		 */
 	}
+	
 	public double ContagionProbability(Person p) {
 		double percent;
 		if(p.getAge()<=18)
@@ -30,6 +39,9 @@ public class ChineseVariant implements IVirus {
 		return percent*p.ContagionProbability();
 	}
 	
+	/*
+	 * @param p - An help method that accepts a patient and returns the probability of death from his virus
+	 */
 	public double p_to_death(Sick p) {
 		if(p.getAge()<18)
 			return 0.001;
@@ -38,7 +50,11 @@ public class ChineseVariant implements IVirus {
 		return 0.05;
 		
 	}
-	
+	 
+	/*
+	 * @param p1 and p2 - A method that accepts 2 people and results in pasting the other with the help of a formula and distance from the first person
+	 */
+	@Override
 	public boolean tryToContagion(Person p1, Person p2) {
 		double distance, min1;
 		if(p2 instanceof Healthy || p2 instanceof Vaccinated || p2 instanceof Convalescent)
@@ -64,6 +80,9 @@ public class ChineseVariant implements IVirus {
 		return false;
 	}
 	
+	/*
+	 * @param p - A method that calculates the probability that the person being transferred will die from the disease
+	 */
 	public boolean tryToKill(Sick p) {
 		double x = p.getVirus().p_to_death(p);
 		double max1 = Math.max(0, x - 0.01*x* Math.pow(Clock.now() - p.getContagiousTime()-15, 2));
@@ -75,6 +94,12 @@ public class ChineseVariant implements IVirus {
 	}
 	public String toString() {
 		return "ChineseVariant";
+	}
+	
+	public boolean equals(Object o) {
+		if (!(o instanceof BritishVariant))
+		   return false;
+		return true;
 	}
 	public static void addMutation(IVirus v)
 	{
